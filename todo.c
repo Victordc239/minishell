a03067378b6b9c7811e1e4a2b4144c2f5e373bfa
Victor Diez <victordc239@gmail.com>
2025-09-11 15:58:02 +0200

    Add files via upload

--- todo.c
+++ todo.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   todo.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: victor <victor@student.42.fr>              +#+  +:+       +#+        */
+/*   By: vdiez-cu <vdiez-cu@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/30 15:25:53 by sofernan          #+#    #+#             */
-/*   Updated: 2025/09/11 12:54:31 by victor           ###   ########.fr       */
+/*   Updated: 2025/09/11 15:57:38 by vdiez-cu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -2173,7 +2173,7 @@ t_env	*create_env_list(char **envp, t_minishell *mini)
 	return (mini->env_list);
 }
 
-static int	execute_group_in_subshell(t_minishell *parent, char *inner)
+/*static int	execute_group_in_subshell(t_minishell *parent, char *inner)	CAMBIADA POR run_group_child Y execute_group_in_subshell
 {
 	pid_t		pid;
 	t_minishell	child;
@@ -2210,6 +2210,53 @@ static int	execute_group_in_subshell(t_minishell *parent, char *inner)
 			ft_freedoom(env_arr);
 		exit(g_status);
 	}
+	if (waitpid(pid, &status, 0) == -1)
+		return (perror("waitpid"), 1);
+	if (WIFEXITED(status))
+		g_status = WEXITSTATUS(status);
+	else if (WIFSIGNALED(status))
+		g_status = 128 + WTERMSIG(status);
+	return (g_status);
+}*/
+static void	run_group_child(t_minishell *parent, char *inner)
+{
+	t_minishell	child;
+	char		**env_arr;
+	char		*status_str;
+
+	signal(SIGINT, SIG_DFL);
+	signal(SIGQUIT, SIG_DFL);
+	child = init_minishell();
+	env_arr = env_to_array(parent->env_list);
+	if (env_arr)
+		child.env_list = create_env_list(env_arr, &child);
+	status_str = ft_itoa(g_status);
+	if (status_str)
+	{
+		add_env_node(&child, "?", status_str, 0);
+		free(status_str);
+	}
+	if (inner)
+		process_input(inner, &child);
+	else
+		process_input("", &child);
+	if (child.env_list)
+		free_env_list(child.env_list);
+	if (env_arr)
+		ft_freedoom(env_arr);
+	exit(g_status);
+}
+
+static int	execute_group_in_subshell(t_minishell *parent, char *inner)
+{
+	pid_t	pid;
+	int		status;
+
+	pid = fork();
+	if (pid == -1)
+		return (perror("fork"), 1);
+	if (pid == 0)
+		run_group_child(parent, inner);
 	if (waitpid(pid, &status, 0) == -1)
 		return (perror("waitpid"), 1);
 	if (WIFEXITED(status))
