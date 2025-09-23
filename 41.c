51a97dcb8c33bf44dee236f6a9e7d8f4986ee063
sorayaafdz <159011568+sorayaafdz@users.noreply.github.com>
2025-09-23 17:11:48 +0200

    Add files via upload

--- 41.c
+++ 41.c
@@ -6,36 +6,83 @@
 /*   By: sofernan <sofernan@student.42madrid.es>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/30 15:25:53 by sofernan          #+#    #+#             */
-/*   Updated: 2025/09/23 14:16:13 by sofernan         ###   ########.fr       */
+/*   Updated: 2025/09/23 17:10:46 by sofernan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "mini.h"
 
-void	append_variable(char **res, char *src, int *i, t_minishell *mini)
+ char	*get_cd_path(t_minishell *mini, int *path_allocated, int *print_new)
 {
-	char	*var;
-	char	*val;
-	int		j;
-	int		len;
-	char	*tmp;
+	char	*path;
 
-	j = *i + 1;
-	while (src[j] && (ft_isalnum(src[j]) || src[j] == '_'))
-		j++;
-	len = j - (*i + 1);
-	tmp = *res;
-	if (len > 0)
+	if (mini->command_list->argv[1] && mini->command_list->argv[2])
+		return (ft_putstr("cd: too many arguments\n", 2), g_status = 1, NULL);
+	if (!mini->command_list->argv[1])
 	{
-		var = ft_substr(src, *i + 1, len);
-		val = env_value(var, mini->env_list);
-		if (!val)
-			val = "";
-		*res = ft_strjoin(tmp, val);
-		free(var);
+		path = get_env_value("HOME", mini->env_list);
+		if (!path)
+			return (ft_putstr("cd: HOME not set\n", 2), g_status = 1, NULL);
+		return (*path_allocated = 1, path);
 	}
-	else
-		*res = ft_strjoin(tmp, "$");
-	free(tmp);
-	*i = j;
+	if (ft_strcmp(mini->command_list->argv[1], "-") == 0)
+	{
+		path = get_env_value("OLDPWD", mini->env_list);
+		if (!path)
+			return (ft_putstr("cd: OLDPWD not set\n", 2), g_status = 1, NULL);
+		return (*path_allocated = 1, *print_new = 1, path);
+	}
+	return (mini->command_list->argv[1]);
+}
+
+ char	*perform_cd(t_minishell *mini, char *path,
+				int path_allocated, char **prev_cwd)
+{
+	char	*new_cwd;
+
+	*prev_cwd = getcwd(NULL, 0);
+	if (!*prev_cwd)
+		*prev_cwd = get_env_value("PWD", mini->env_list);
+	if (chdir(path) == -1)
+	{
+		perror("cd");
+		g_status = 1;
+		if (path_allocated)
+			free(path);
+		if (*prev_cwd)
+			free(*prev_cwd);
+		return (NULL);
+	}
+	g_status = 0;
+	new_cwd = getcwd(NULL, 0);
+	if (!new_cwd)
+		new_cwd = ft_strdup(path ? path : "");
+	return (new_cwd);
+}
+
+void	ft_cd(t_minishell *mini, int path_allocated, int print_new)
+{
+	char	*path;
+	char	*prev_cwd;
+	char	*new_cwd;
+
+	path = get_cd_path(mini, &path_allocated, &print_new);
+	if (!path)
+		return ;
+	new_cwd = perform_cd(mini, path, path_allocated, &prev_cwd);
+	if (!new_cwd)
+		return ;
+	if (prev_cwd)
+	{
+		if (!update_node(mini->env_list, "OLDPWD", prev_cwd, 1))
+			add_env_node(mini, "OLDPWD", prev_cwd, 1);
+		free(prev_cwd);
+	}
+	if (!update_node(mini->env_list, "PWD", new_cwd, 1))
+		add_env_node(mini, "PWD", new_cwd, 1);
+	if (print_new)
+		ft_putstr_fd(new_cwd, 1);
+	free(new_cwd);
+	if (path_allocated)
+		free(path);
 }
